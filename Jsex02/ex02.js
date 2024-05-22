addc330dc5072c1e2eee149a6f9371bc95b670d6
Glow <138259022+PedroGranato@users.noreply.github.com>
2024-05-22 14:53:45 +0000

    finish 3º lesson

--- Jsex02/ex02.js
+++ Jsex02/ex02.js
@@ -1,22 +1,55 @@
-function send() {
-    const Name = document.getElementById('Name').value;
-    const CPF = document.getElementById('CPF').value;
-    const Date = document.getElementById('Date').value;
-    const CEP = document.getElementById('CEP').value;
-    const Tel = document.getElementById('Tel').value;
-
-    localStorage.setItem('Name', Name);
-    localStorage.setItem('CPF', CPF);
-    localStorage.setItem('Date', Date);
-    localStorage.setItem('CEP', CEP);
-    localStorage.setItem('Tel', Tel);
-
-document.body.innerHTML = "<div class='result'>" +
-"<h1 class='Tittle'> Resultado </h1>" + 
-"<h3> Nome:"  + localStorage.getItem('Name') + "</h3>" + 
-"<h3> CPF: " + localStorage.getItem('CPF') + "</h3>" + 
-"<h3> Data: " + localStorage.getItem('Date') + "</h3>" + 
-"<h3> CEP: " + localStorage.getItem('CEP') + "</h3>" + 
-"<h3> Telefone: " + localStorage.getItem('Tel') + "</h3>" + 
-"</div>";
+// Objeto para armazenar as quantidades iniciais dos produtos
+let productQuantities = {
+    Sucrilhos: 0,
+    Oreo: 0,
+    Moca: 0,
+    Arroz: 0,
+    Feijao: 0,
+    Leite: 0,
+    Margarina: 0,
+    Requeijao: 0,
+    Queijo: 0,
+    Detergente: 0
 };
+
+window.onload = function() {
+    for (let product in productQuantities) {
+        let storedValue = localStorage.getItem(product);
+        
+        if (storedValue !== null) {
+            productQuantities[product] = parseInt(storedValue);
+        } else {
+            localStorage.setItem(product, 0);
+        }
+        document.getElementById(`count-${product}`).textContent = productQuantities[product];
+    }
+}
+
+
+function updateProductCount(productId, increment) {
+    if (increment) {
+        productQuantities[productId]++;
+    } else {
+        if (productQuantities[productId] > 0) {
+            productQuantities[productId]--;
+        }
+    }
+    
+    localStorage.setItem(productId, productQuantities[productId]);
+    document.getElementById(`count-${productId}`).textContent = productQuantities[productId];
+}
+
+
+document.querySelectorAll('.button-add').forEach(button => {
+    button.addEventListener('click', () => {
+        const productId = button.getAttribute('productId');
+        updateProductCount(productId, true);
+    });
+});
+
+document.querySelectorAll('.button-remove').forEach(button => {
+    button.addEventListener('click', () => {
+        const productId = button.getAttribute('productId');
+        updateProductCount(productId, false);
+    });
+});
